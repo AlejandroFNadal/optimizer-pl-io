cd683ff3783d1746aed2e3e4f6acf7fd850626ff
AnastaciusWright <alexandronadal@gmail.com>
2020-08-20 17:33:24 -0300

    Comentarios, y los parentesis en el split join al hacer reemplazos

--- src/components/NoLinealProgramming/Methods/Gradiente.js
+++ src/components/NoLinealProgramming/Methods/Gradiente.js
@@ -39,47 +39,59 @@ const fGradiente = (funcionObjetivo,puntoa,puntob,e,Objetivo) => {
      valorR = 1;
      var salida = 0;
 
-     // Calculo las derivadas de la funcion en x y en y
+     // Derivo la funcion respecto a  x y respecto a y
      derivadaExpr = [ math.derivative(expr.toString(),'x') , math.derivative(expr.toString(),'y')];
-
+     //al principio, la primera comparacion es 1>0 y salida arranca en 0
      while (((math.abs(valorR)) > epsilon) && (salida < 999)){
           epsilon=e;
           // La variable salida representa la condicion en la que el punto se encuentra en el infinito
           salida = salida + 1;
 
-          // Reemplazo x0 en las derivas 
+          // Evaluo x0 en las derivadas 
           deltaf = [ derivadaExpr[0].evaluate({x: x0[0], y: x0[1]}) , derivadaExpr[1].evaluate({x: x0[1], y: x0[1]})];
-
+          console.log("Deltaf tras la evaluacion de las derivadas en x0")
+          console.log(deltaf)
+          /*
+          //Este parseo, no lo dudes, funciona
           if (math.abs(parseFloat(deltaf[0].toString())) < 0.0001){
                deltaf[0]=0;
           }
           if (math.abs(parseFloat(deltaf[1].toString())) < 0.0001){
                deltaf[1]=0;
           }
+          */
           if (objetivo=='max'){
                deltaf = ['('+deltaf[0]+'*r)','('+deltaf[1]+'*r)'];
           }else{
                deltaf = ['-('+deltaf[0]+'*r)','-('+deltaf[1]+'*r)'];
           }
+          console.log("deltaf tras sumarle el caracter r")
+          console.log(deltaf)
           deltaf = [ math.simplify(deltaf[0]) , math.simplify(deltaf[1]) ];
-
+          console.log("deltaf tras la simplificacion")
+          console.log(deltaf.toString())
           // Genero el punto X1 el cual contiene una variable r que despues tendremos que despejar
           if (x0[0]===0){
                x1conR[0]='('+deltaf[0]+')';
           }else{
-               x1conR[0]=x0[0]+'('+deltaf[0]+')';
+               x1conR[0]=x0[0]+'+('+deltaf[0]+')';
           }
-
+          console.log("x1conR")
+          console.log(x1conR)
           if (x0[1]===0){
                x1conR[1]='('+deltaf[1]+')';
           }else{
-               x1conR[1]=x0[1]+'('+deltaf[1]+')';
+               x1conR[1]=x0[1]+'+('+deltaf[1]+')';
           }
+          console.log("x1conR")
+          console.log(x1conR)
           //x1conR = [ x0[0]+'('+deltaf[0]+')' , x0[1] +'('+ deltaf[1]+')' ];
           x1conR = [ math.simplify(x1conR[0]) , math.simplify(x1conR[1]) ];
 
           x1conR = [ (x1conR[0].toString()) , (x1conR[1].toString()) ];
 
+          console.log("x1con R tras la simplificacion")
+          console.log(x1conR)
           // Aca se corrige el error de que por algun motivo elimina el simbolo *
           if (x1conR[0].match(regder) != null)
           {
@@ -98,28 +110,46 @@ const fGradiente = (funcionObjetivo,puntoa,puntob,e,Objetivo) => {
           {
                x1conR[1]=x1conR[1].split("r ").join("r *");
           }
-          
+          console.log("x1conR tras el split join de r evaluado con las regexp")
+          console.log(x1conR)
           //x1conR = [ x1conR[0].split("r").join("* r") , x1conR[1].split("r").join("* r") ];
           //x1conR = [ x1conR[0].split("* *").join("*") , x1conR[1].split("* *").join("*") ];
 
           // Se simplifica para que sea mas facil realizar la wea
           x1conR = [ math.simplify(x1conR[0]) , math.simplify(x1conR[1]) ];
-
+          console.log("Simplificacion de x1 tras el split join")
+          console.log(x1conR.toString())
 
 
           // Reemplazo los valores de X1 en la funcion objetivo y luego despejo r
+          console.log(Z)
+          Z = Z.split("x").join("(x)")
+          Z = Z.split("y").join("(y)")
           ZconR = Z.split("x").join(x1conR[0]);
           ZconR = ZconR.split("y").join(x1conR[1]);
-
+          console.log("ZconR")
+          console.log(ZconR)
           if ( (ZconR).includes("r") ){
                //Aca se despejaria r pero nada anda
                helper = ZconR.split("r").join("x");
                helper = math.simplify(helper);
+               console.log("ZconR reemplazado r por x")
+               console.log(helper.toString())
                helper = (helper.toString()).split("+ -").join("-");
+               helper = (helper.toString()).split("+-").join("-");
                valorR = algebrite.nroots(helper.toString());
-
+               console.log("raices de helper")
+               console.log(valorR.toString())
+               var primerElemento = valorR[0];
+               console.log(primerElemento)
                valorR = valorR.toString()
+
+               //Su nivel de suciedad es insuperable
                valorR = valorR[1]+valorR[2]+valorR[3]+valorR[4]+valorR[5]+valorR[6]+valorR[7]
+               console.log("valorR en 2")
+               console.log(valorR[2])
+               console.log("Despues de la suciedad")
+               console.log(valorR)
                valorR = parseFloat(valorR);
           } else {
                valorR = 0;
@@ -144,5 +174,5 @@ const fGradiente = (funcionObjetivo,puntoa,puntob,e,Objetivo) => {
 
 
 }
-
+fGradiente("4*x+6*y-2*x^2-2*x*y-2*x^2",1,1,0.1,"max")
 module.exports = fGradiente
